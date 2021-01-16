1f1189927e451745d4dc3164f6c67d564eb4d280
Fellainest <filonov.4@mail.ru>
2021-01-17 01:06:27 +0700

    published

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const fetch = require('node-fetch');
 const endpoints = require('./src/endpoints')
 
 exports.cry = async function() {
-    let url = endpoints.kaneki.url + 'cry'
-    let kaneka = await fetch(url).then(r=> r.json())
+    let kaneka = await fetch(endpoints.kaneki.url + 'cry').then(r=> r.json())
     return { url: kaneka.url };
 };
 
@@ -11,16 +10,14 @@ exports.naruto = async function(number) {
     if (typeof number !== 'number') {
         throw new TypeError('Укажите число');
     }
-    if(number < 0 || number === 0) throw new TypeError('Томов не может быть меньше чем 1')
-    if(number > 700) throw new Error('Томов пока только 700. Напишите число от 1-700')
-    let url = endpoints.kaneki.url + `naruto/${number}`
-    let kaneka = await fetch(url).then(r=> r.json())
+    if(number < 0 || number === 0) throw new TypeError('Страниц не может быть меньше чем 1')
+    if(number > 700) throw new Error('Напишите число от 1-700')
+    let kaneka = await fetch(endpoints.kaneki.url + `naruto/${number}`).then(r=> r.json())
     return kaneka;
 };
 
 exports.clever = async function(string) {
-    let url = endpoints.kaneki.url + `clever`
-    let kaneka = await fetch(url, {
+    let kaneka = await fetch(endpoints.kaneki.url + `clever`, {
         method: 'POST',
         body:  JSON.stringify({ query: string }),
         headers: { 'content-type': 'application/json' }
