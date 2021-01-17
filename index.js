11e4c35c5eb216ea9d93b28d73cd8512bece1efa
Fellainest <filonov.4@mail.ru>
2021-01-17 11:46:19 +0700

    published

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetch = require('../src/fetch.js');
 const endpoints = require('./src/endpoints')
 
 exports.cry = async function() {
-    let kaneka = await fetch(endpoints.kaneki.url + 'cry').then(r=> r.json())
+    let kaneka = await fetch(endpoints.kaneki.url + 'cry')
     return { url: kaneka.url };
 };
 
@@ -12,11 +12,11 @@ exports.naruto = async function(number) {
     }
     if(number < 0 || number === 0) throw new TypeError('Страниц не может быть меньше чем 1')
     if(number > 700) throw new Error('Напишите число от 1-700')
-    let kaneka = await fetch(endpoints.kaneki.url + `naruto/${number}`).then(r=> r.json())
+    let kaneka = await fetch(endpoints.kaneki.url + `naruto/${number}`)
     return kaneka;
 };
 
 exports.clever = async function(string) {
-    let kaneka = await fetch(endpoints.kaneki.url + `clever`, {json: { query: string } }).then(r=> r.json())
+    let kaneka = await fetch(endpoints.kaneki.url + `clever`, {json: { query: string } })
     return { message: kaneka.message, context: kaneka.contexts};
 };
